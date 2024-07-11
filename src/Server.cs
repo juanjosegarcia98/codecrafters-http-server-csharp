285e0ad6c1682326025856c25cf22003fb2edc3b
Juan José García <juanjgar998@gmail.com>
2024-07-11 19:46:57 -0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -2,11 +2,52 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+const string localhostAddress = "127.0.0.1";
+const int port = 4221;
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
+const string ROOT_PATH = "/";
+
+const string OK = "HTTP/1.1 200 OK\r\n\r\n";
+const string NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+IPAddress localIpAddress = IPAddress.Parse(localhostAddress);
+byte[] buffer = new byte[1024];
+
+TcpListener server = new(localIpAddress, port);
+
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+Console.WriteLine($"Server listening on http://{localIpAddress}:{port}");
+
+while (true)
+{
+    Socket socket = server.AcceptSocket();
+
+    handleRequest(socket);
+
+    socket.Close();
+}
+
+void handleRequest(Socket socket)
+{
+    int bytesReceived = socket.Receive(buffer);
+    string request = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+    string path = extractUrlPathFromRequest(request);
+    byte[] responseBytes = path switch
+    {
+        ROOT_PATH => Encoding.ASCII.GetBytes(OK),
+        _ => Encoding.ASCII.GetBytes(NOT_FOUND),
+    };
+
+    socket.Send(responseBytes);
+}
+
+static string extractUrlPathFromRequest(string request)
+{
+    string[] lines = request.Split('\n');
+    if (lines.Length == 0) return ROOT_PATH;
+
+    string[] parts = lines[0].Split(' ');
+    if (parts.Length < 2) return ROOT_PATH;
+    
+    return parts[1];
+}
