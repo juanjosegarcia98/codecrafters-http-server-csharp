32b0cf0d6157729a30f148d3c4616627c79bb8d8
Juan José García <juanjgar998@gmail.com>
2024-07-15 12:00:46 -0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -3,24 +3,28 @@ using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
 
-const string localhostAddress = "127.0.0.1";
-const int port = 4221;
+const string LOCALHOST_ADDRESS = "127.0.0.1";
+const int PORT = 4221;
 
-const string ROOT_PATH = @"^/$";
-const string ECHO_PATH = @"/echo/([^/]+)";
-
-string[] routes = [ROOT_PATH, ECHO_PATH];
+const string ROOT_ROUTE = @"^/$";
+const string ECHO_ROUTE = @"/echo/([^/]+)";
+const string USER_AGENT_ROUTE = @"^/user-agent$";
 
 const string OK = "HTTP/1.1 200 OK\r\n\r\n";
-string OK_BODY_TEXT(string body) => $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {body.Length}\r\n\r\n{body}";
 const string NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-IPAddress localIpAddress = IPAddress.Parse(localhostAddress);
+const string REQUEST_HEADER_PATTERN = @"^([\w-]+):\s*(.+)$";
+
+string okBodyResponse(string body) => $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {body.Length}\r\n\r\n{body}";
+
+string[] serverRoutes = [ROOT_ROUTE, ECHO_ROUTE, USER_AGENT_ROUTE];
+
+IPAddress localIpAddress = IPAddress.Parse(LOCALHOST_ADDRESS);
 byte[] buffer = new byte[1024];
 
-TcpListener server = new(localIpAddress, port);
+TcpListener server = new(localIpAddress, PORT);
 server.Start();
-Console.WriteLine($"Server listening on http://{localIpAddress}:{port}");
+Console.WriteLine($"Server listening on http://{localIpAddress}:{PORT}");
 
 while (true)
 {
@@ -35,18 +39,24 @@ void handleRequest(Socket socket)
 {
     int bytesReceived = socket.Receive(buffer);
     string request = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+
+    Dictionary<string, string> requestHeaders = extractHeadersFromRequest(request);
     
     string requestPath = extractUrlPathFromRequest(request);
 
-    string? routeMatched = routes.Where(route => Regex.Match(requestPath, route).Success).FirstOrDefault(defaultValue: null);
+    string? routeMatched = serverRoutes.Where(route => Regex.Match(requestPath, route).Success).FirstOrDefault(defaultValue: null);
 
     switch (routeMatched)
     {
-        case ROOT_PATH:
+        case ROOT_ROUTE:
             handleRootRoute(socket);
             break;
-        case ECHO_PATH:
-            handleEchoRoute(socket, requestPath);
+        case ECHO_ROUTE:
+            var pathParameters = extractPathParametersFromUrl(requestPath, ECHO_ROUTE);
+            handleEchoRoute(socket, pathParameters[1]);
+            break;
+        case USER_AGENT_ROUTE:
+            handleUserAgentRoute(socket, requestHeaders["User-Agent"]);
             break;
         default:
             handleNotFoundRoute(socket);
@@ -54,26 +64,63 @@ void handleRequest(Socket socket)
     }
 }
 
-static string extractUrlPathFromRequest(string request)
+void handleNotFoundRoute(Socket socket)
 {
-    string[] lines = request.Split('\n');
-    if (lines.Length == 0) return ROOT_PATH;
-
-    string[] parts = lines[0].Split(' ');
-    if (parts.Length < 2) return ROOT_PATH;
-    
-    return parts[1];
-}
-
-void handleNotFoundRoute(Socket socket) {
     socket.Send(Encoding.ASCII.GetBytes(NOT_FOUND));
 }
 
-void handleRootRoute(Socket socket) {
+void handleRootRoute(Socket socket)
+{
     socket.Send(Encoding.ASCII.GetBytes(OK));
 }
 
-void handleEchoRoute(Socket socket, string requestPath) {
-    string message = Regex.Match(requestPath, ECHO_PATH).Groups[1].Value;
-    socket.Send(Encoding.ASCII.GetBytes(OK_BODY_TEXT(message)));
+void handleEchoRoute(Socket socket, string body)
+{
+    socket.Send(Encoding.ASCII.GetBytes(okBodyResponse(body)));
+}
+
+void handleUserAgentRoute(Socket socket, string userAgent)
+{
+    socket.Send(Encoding.ASCII.GetBytes(okBodyResponse(userAgent)));
+}
+
+static string extractUrlPathFromRequest(string request)
+{
+    string[] lines = request.Split('\n');
+    if (lines.Length == 0) return ROOT_ROUTE;
+
+    string[] parts = lines[0].Split(' ');
+    if (parts.Length < 2) return ROOT_ROUTE;
+
+    return parts[1];
+}
+
+static Dictionary<int, string> extractPathParametersFromUrl(string urlPath, string pattern)
+{
+    Dictionary<int, string> pathParameters = new Dictionary<int, string>();
+
+    Match match = Regex.Match(urlPath, pattern);
+
+    for (int i = 1; i <= match.Groups.Count; i++)
+    {
+        pathParameters[i] = match.Groups[i].Value;
+    }
+
+    return pathParameters;
+}
+
+static Dictionary<string, string> extractHeadersFromRequest(string request)
+{
+    Dictionary<string, string> headers = new Dictionary<string, string>();
+
+    MatchCollection matches = Regex.Matches(request, REQUEST_HEADER_PATTERN, RegexOptions.Multiline);
+
+    foreach (Match match in matches)
+    {
+        string key = match.Groups[1].Value;
+        string value = match.Groups[2].Value;
+        headers[key] = value.Remove(value.Length - 1);
+    }
+
+    return headers;
 }
